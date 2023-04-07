a9c56a1a1a3af70590f06520430bb2f81c86b170
emerguns <emerguns@gmail.com>
2023-04-07 18:02:48 -0500

    "implemente axios y cree gitignore"

--- app.js
+++ app.js
@@ -1,70 +1,65 @@
-const bodyParser = require("body-parser");
 const express = require("express");
-const request = require("request");
-const https = require("https");
+const axios = require("axios");
 
-const app = express ();
+const app = express();
 
-app.use(express.static("public")); // para dirigir a los archivos de css e imagenes
-app.use(bodyParser.urlencoded({extended: true})); // eso se nececita para usar body parser 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html")
+  res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/", function(req, res){
+  const firstName = req.body.fName; 
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-    const firstName = req.body.fName; 
-    const lastName = req.body.lName;
-    const email = req.body.email;
-
-    const data = {
-        members: [
-            {
-            email_address: email,
-            status: "subscribed",
-            merge_fields: {
-                FNAME: firstName,
-                LNAME: lastName
-            }
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
         }
-        ]
-    };
+      }
+    ]
+  };
 
-    const jsonData = JSON.stringify(data);
+  const url = " url mailchimp";
 
-    const url = "https://us8.api.mailchimp.com/3.0/lists/226a2da49d";
-
-    const options = {
-        method: "POST",
-        auth: "emerson1:68d0a0953b66473df6855a0739c37a39-us8"
+  const options = {
+    auth: {
+      username: "emerson1",
+      password: "API"
     }
+  };
 
-    const request = https.request(url, options, function(response){
-
-        if (response.statusCode === 200) {
-            res.sendFile(__dirname + "/success.html")
-        }else{
-            res.sendFile(__dirname + "/failure.html")
-        }
-
-        response.on("data", function(data){
-            console.log(JSON.parse(data));
-        })
+  axios.post(url, data, options)
+    .then(function(response){
+      if (response.status === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+      console.log(response.data);
+    })
+    .catch(function(error){
+      console.log(error);
+      res.sendFile(__dirname + "/failure.html");
     });
-
-    request.write(jsonData);
-    request.end();
-
 });
 
 app.post("/failure", function(req, res){
-    res.redirect("/");
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
-    console.log("servidor corriendo en el puerto 3000");
+  console.log("Server is running on port 3000");
 });
 
-//API key == 68d0a0953b66473df6855a0739c37a39-us8 
-//list id == 226a2da49d 
+
+//API key == 9c1fd4aec609c3c5e97a8d81891787ac-us8 
+//list id == 0e568b82fa 
